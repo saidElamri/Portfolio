31880bb1d960447673d2c9c3cc299228cc01e48e
SaidElamri <said.elamri.evil>
2024-11-21 13:39:13 +0100

    added portfolio project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import '../styles/Projects.css'; // Import the CSS file for styling
 
 const projects = [
-  { title: 'MLS Quiz App', description: 'A quiz platform with React and MongoDB.', tech: 'MERN', link: '#', img: './pro.jpg' },
-  { title: 'ODCStream', description: 'A platform for managing online meetings.', tech: 'React, Node.js', link: '#', img: './pro.jpg' },
+  { title: 'MLS Quiz App', description: 'A quiz platform with React and MongoDB.', tech: 'MERN', link: 'https://github.com/saidElamri/Quizz-LMS', img: './lms-overview.png' },
+  { title: 'My-portfolio', description: ' created this portfolio to showcase my journey as a web developer, featuring my projects and skills in technologies like React, JavaScript, and CSS, highlighting my passion for building innovative web solutions', tech: 'React, Node.js', link: 'https://github.com/saidElamri/Portfolio', img: './portfolio.png.png' },
   { title: 'Little Lemon Booking', description: 'A restaurant booking form with Django and MySQL.', tech: 'Django, MySQL', link: '#', img: './pro.jpg' },
 ];
 
